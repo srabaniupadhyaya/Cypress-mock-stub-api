696bce46a95d58f543bc9cbde13dfcfabebd3293
Srabani <srabaniupadhyaya@yahoo.com>
2024-07-20 19:50:24 +0100

    comments update

--- cypress/e2e/mock-stub-api.cy.js
+++ cypress/e2e/mock-stub-api.cy.js
@@ -38,7 +38,7 @@ describe('Stub and mock scenarios', () => {
               cy.visit('https://dummyapi.io/explorer')
             cy.intercept({
               path:'/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10'},
-             items
+             items//Using the fixture data
             ).as('commentsResponse')
             cy.get('.flex :nth-child(5)').click()
               cy.wait('@commentsResponse').then(interResponse=>
